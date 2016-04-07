fe77e1ed4f3fb8cb8a226b00a6d160f596dfdae3
Brian Sheaffer <briansheaffer@hotmail.com>
2016-04-07 10:43:26 -0700

    Typos and Splat the Select properties
    Couple typos (although, i don't need the spaces in the Axx fields, so
    those weren't technically typos).
    
    I also splat the property list for Select-Object. Makes maintaining it
    simpler (someday, we'll revisit removing the Axx fields).

--- Work/TEM-ADQuerying/tem.ps1
+++ Work/TEM-ADQuerying/tem.ps1
@@ -4,7 +4,7 @@
     Creates the Telephone Expense CSV list
 
 .DESCRIPTION
-    Queries AD for all (applicable) users, and outputs the data into the tem.csv file. Accpunts must meet the following criteria:
+    Queries AD for all (applicable) users, and outputs the data into the tem.csv file. Accounts must meet the following criteria:
         Must be a user
         Must be an "organizational person"
         Must have an employe ID
@@ -35,7 +35,7 @@ param (
 Function Get-Users {
     #This is the Filter so we only pull the right info from AD
     #Only return: User and orgperson, with something in the EmpID, GivenName, and ExtenisonAtt5 fields
-    #But exclue any account that ends in Admin
+    #But exclude any account that ends in Admin
     $sFilter = "(&(objectClass=User)(objectcategory=organizationalperson)(employeeID=*)(givenname=*)(extensionAttribute5=*)(!(sAMAccountName=*admin)))"
     
     #This is the domain object
@@ -55,10 +55,10 @@ Function Get-Users {
         $null = $oSearcher.PropertiesToLoad.Add($i)
     }
     
-    #Now, pull them all in
+    #Now, search and collect the results
     $cResults = $oSearcher.FindAll()
     
-    #And cycle through them
+    #And cycle through the results to assemble our output object
     foreach ($oResult in $cResults) {
         #usrAccountControl holds the 'enabled'/'disabled' status of an account
         #we bitwise and it against 2 to see which...
@@ -85,38 +85,38 @@ Function Get-Users {
             sn = $oResult.Properties.Item("sn")[0]
             givenName = $oResult.Properties.Item("givenName")[0]
             mail = $oResult.Properties.Item("mail")[0]
-            A3 = " "
+            A3 = ""
             extensionAttribute5 = $oResult.Properties.Item("extensionAttribute5")[0]
             A4 = "Y"
             status = $uac
-            A5 = " "
-            A6 = " "
-            A7 = " "
+            A5 = ""
+            A6 = ""
+            A7 = ""
             hiredate = $hire.ToString("yyyy-MM-dd")
-            A8 = " "
-            A9 = " "
-            A10 = " "
-            A11 = " "
-            A12 = " "
-            A13 = " "
-            A14 = " "
-            A15 = " "
+            A8 = ""
+            A9 = ""
+            A10 = ""
+            A11 = ""
+            A12 = ""
+            A13 = ""
+            A14 = ""
+            A15 = ""
             A16	= "N"
             A17	= "N"
             A18	= "N"
-            A19 = " "
-            A20 = " "
-            A21 = " "
-            A22 = " "
-            A23 = " "	
-            A24 = " "
+            A19 = ""
+            A20 = ""
+            A21 = ""
+            A22 = ""
+            A23 = ""	
+            A24 = ""
             A25 = "0"
         }
         
         #Create a new object for this info
         $InfoStack = New-Object -TypeName PSObject -Property $InfoHash
     
-        #Add a (hopefully) unique object type name
+        #Add a unique object type name
         $InfoStack.PSTypeNames.Insert(0,"TEM.Users")
 
         #And output the object
@@ -124,12 +124,29 @@ Function Get-Users {
     }
 }
 
+#For re-use, I splat the -Property attrubute of the Select-Object cmdlet below
+#This ensures the output has the fields in the correct order
+$Selection = @{ 
+    'Property' =    'samAccountName',
+                    'A1',
+                    'sn', 
+                    'givenName', 
+                    'mail', 
+                    'A3', 
+                    'extensionAttribute5', 
+                    'A4',
+                    'status', 
+                    'A5', 'A6', 'A7', 
+                    'Hiredate', 
+                    'A8', 'A9', 'A10', 'A11', 'A12', 'A13', 'A14', 'A15', 'A16', 'A17', 'A18', 'A19', 'A20', 'A21', 'A22', 'A23', 'A24', 'A25'
+}
 
 
-
+#Now, save or display the info
+#We select the objects with the right properties, sort them by username, convert to csv, remove the double-quotes, and skip the first 2 lines
 if ($DisplayOnly) {
     Get-Users | 
-        select samAccountName, A1, sn, givenName, mail, A3, extensionAttribute5, A4, status, A5, A6, A7, Hiredate, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20, A21, A22, A23, A24, A25 | 
+        Select-Object @Selection | 
         sort samaccountName | 
         ConvertTo-Csv | 
         % {$_ -replace '"',''} |
@@ -137,11 +154,11 @@ if ($DisplayOnly) {
 }
 else {
     Get-Users | 
-        select samAccountName, A1, sn, givenName, mail, A3, extensionAttribute5, A4, status, A5, A6, A7, Hiredate, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20, A21, A22, A23, A24, A25 | 
+        Select-Object @Selection |
         sort samaccountName | 
         ConvertTo-Csv | 
         % {$_ -replace '"',''} |
         select -skip 2 | 
-        Out-File TEM.csv
+        Out-File tem.csv
 }
 
